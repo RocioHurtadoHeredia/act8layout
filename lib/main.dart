830f578d1f38a4577ef39e99156d61fe52d49c35
ROCIO_ALEJANDRA_HURTADO_HEREDIA <a.20308051281009@cbtis128.edu.mx>
2023-04-25 16:41:44 +0000

    verison1

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,132 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+// Uncomment lines 3 and 6 to view the visual layout at runtime.
+// import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
 
-void main() => runApp(MyApp());
+void main() {
+  // debugPaintSizeEnabled = true;
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            /*1*/
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                /*2*/
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: const Text(
+                    'Carls Jr Las Torres',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  'Av. Las Torres.1590 Locales F y G, Parque Industrial, Intermex, 32575 Cd Juárez, Chih.',
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          /*3*/
+          const Icon(
+            Icons.star,
+            color: Color(0xffffcc3b),
+          ),
+          const Text('41'),
+        ],
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
-  }
-}
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+    Color color = const Color(0xffea9800);
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.call, 'Llamar'),
+        _buildButtonColumn(color, Icons.near_me, 'Ruta'),
+        _buildButtonColumn(color, Icons.share, 'Compartir'),
+      ],
+    );
+
+    Widget textSection = const Padding(
+      padding: EdgeInsets.all(32),
+      child: Text(
+        'Carls Jr. es una cadena de restaurante de comida rápida originada en los Estados Unidos y actualmente presente en el resto del mundo. Fue fundada en 1941 por Carl Karcher y es propietario de CKE Restaurants. Sus mayores competidores son McDonalds, Burger King y Wendys.',
+        softWrap: true,
+        textAlign: TextAlign.justify,
       ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+    );
+
+    return MaterialApp(
+      title: 'Carls Jr',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Carls Jr'),
+          centerTitle: true,
+          backgroundColor: const Color(0xff000000),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(
+                Icons.more_vert,
+              ),
+              onPressed: () {},
+            )
+          ],
+        ),
+        body: ListView(
+          children: [
+            Image.network(
+              'https://cdn.ntmx.me/media/2022/04/13/_hd4e049e7a48c23b2f76028361977399356ae52242.jpg',
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover,
+            ),
+            titleSection,
+            buttonSection,
+            textSection,
+          ],
         ),
       ),
+    );
+  }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
